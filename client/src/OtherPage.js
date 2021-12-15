b6680db838b9badd315a554ed98bd9f42fdcd6ec
miguel cruz <mcruzlehardy@usnwomcruzleh1.lan>
2021-12-15 13:58:05 -0600

    initial commit

---
+++ client/src/OtherPage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// export default () => {
+//     return (
+//         <div>
+//             Im some other page!
+//             <Link to="/">Go back home</Link>
+//         </div>
+//     )
+// }
+
+function OtherPage() {
+    return (
+        <div>
+            Im some other page!
+            <Link to="/">Go back home</Link>
+        </div>
+    )
+}
+export default OtherPage;
